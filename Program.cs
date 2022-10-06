7a459d45770a422299c967abfbcac6d18390f4e6
allsec <allsec@ASTLPTWFH116>
2022-10-06 12:34:36 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello,World");
+//WriteLine is similar Like printf
+Console.WriteLine("Allsec Team Started .Net Framework");
+Console.WriteLine("Let try few arithmetic operators output");
+int a = 100;
+int b = 20;
+int c = a + b;
+Console.WriteLine(c);
+
